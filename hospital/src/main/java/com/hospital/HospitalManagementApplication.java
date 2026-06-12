3caa4201ff95ed810c238ea95bf3ecc57e04d4ae
diwakar8939 <diwakar98765432@gmail.com>
2026-06-12 15:14:12 +0530

    Front-end added

--- hospital/src/main/java/com/hospital/HospitalManagementApplication.java
+++ hospital/src/main/java/com/hospital/HospitalManagementApplication.java
@@ -3,7 +3,6 @@ package com.hospital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication = marks this as the starting point of the Spring Boot app
 @SpringBootApplication
 public class HospitalManagementApplication {
 
